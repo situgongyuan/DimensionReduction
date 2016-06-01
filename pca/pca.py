66f875c4d725c01202faf6160d66e71b7207bdcf
stgy <situgongyuan@gmail.com>
2016-06-01 08:34:53 +0800

    adding simple print testing

--- pca/pca.py
+++ pca/pca.py
@@ -17,6 +17,8 @@ def eig_pca(X,n_dims):
 pca by singular value decomposition
 """
 def svd_pca(X,n_dims):
+    mean = np.mean(X, 0)
+    X = X - mean
     U,Sigma,Vt = np.linalg.svd(X)
     V = Vt.T
     Y = np.dot(X,V[:,0:n_dims])
@@ -25,26 +27,37 @@ def svd_pca(X,n_dims):
 """
 pca by power iteration
 """
-def power_pca(X,n_dims,max_iter=100):
+def power_pca(X,n_dims,max_iter=1000):
     (N,D) = X.shape
     mean = np.mean(X, 0)
     X = X - mean
     cov = np.dot(X.T, X) / N
-    eigen_vectors = np.zeros(D,n_dims)
+    eigen_vectors = np.zeros((D,n_dims))
     for i in range(n_dims):
         p = np.random.rand(1,D)
         p = p / np.sqrt(np.sum(p ** 2))
         p = p.reshape(D,1)
         for j in range(max_iter):
-            q = np.dot(cov,p)
-            p = q / np.sqrt(np.sum(p ** 2))
-        eigen_vectors[:,i] = p
+            p = np.dot(cov,p)
+            p = p / np.sqrt(np.sum(p ** 2))
+        eigen_vectors[:,i] = p.reshape(D,)
         eigen_value = np.dot(np.dot(p.T,cov),p)
+        #print eigen_value
         cov = cov - eigen_value * np.dot(p,p.T)
     Y = np.dot(X,eigen_vectors)
     return Y
 
 
+if __name__ == "__main__":
+    X = np.random.rand(20,10)
+    Y1 = eig_pca(X,2)
+    print Y1
+    Y2 = svd_pca(X,2)
+    print Y2
+    Y3 = power_pca(X,2)
+    print Y3
+
+    
 
 
 
